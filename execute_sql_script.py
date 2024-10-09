2a095333e7a6572715e750142d0d3a9f8bdbad24
ramanpreetk197 <ramanpreetk197@gmail.com>
2024-10-08 21:49:33 -0400

    Create execute_sql_script.py

---
+++ execute_sql_script.py
@@ -0,0 +1,29 @@
+
+
+import mysql.connector
+import os
+
+# Database connection
+connection = mysql.connector.connect(
+    host=os.getenv('MYSQL_HOST'),
+    user=os.getenv('MYSQL_USER'),
+    password=os.getenv('MYSQL_PASSWORD'),
+    database=os.getenv('MYSQL_DB')
+)
+
+cursor = connection.cursor()
+
+# Open and execute the SQL script
+with open('add_departments.sql', 'r') as file:
+    sql_script = file.read()
+    sql_commands = sql_script.split(';')
+    
+    for command in sql_commands:
+        if command.strip():
+            cursor.execute(command)
+
+connection.commit()
+cursor.close()
+connection.close()
+
+print("SQL script executed successfully.......")
